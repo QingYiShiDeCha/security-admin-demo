3eb41c456133cdf346556eaa514e912a8a7aea12
QingCha <2521541557@qq.com>
2024-09-16 22:42:01 +0800

    feat✨: 自定义锁定异常

---
+++ Security-Admin-java/src/main/java/org/qingcha/security/common/exception/UserCountLockException.java
@@ -0,0 +1,13 @@
+package org.qingcha.security.common.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UserCountLockException extends AuthenticationException {
+    public UserCountLockException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public UserCountLockException(String msg) {
+        super(msg);
+    }
+}
