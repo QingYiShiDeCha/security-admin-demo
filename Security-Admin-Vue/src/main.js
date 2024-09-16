ccf914753b6105ab4baee233d34979a37e8d9a37
QingCha <2521541557@qq.com>
2024-09-16 16:41:00 +0800

    feat✨: svg-icon 全局配置

--- Security-Admin-Vue/src/main.js
+++ Security-Admin-Vue/src/main.js
@@ -1,16 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
-
+import 'virtual:svg-icons-register'
+import globalComponent from '@/components'
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
+app.use(globalComponent)
 app.mount('#app')
--- Security-Admin-Vue/vite.config.js
+++ Security-Admin-Vue/vite.config.js
@@ -1,14 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons/svg')],
+      symbolId: "icon-[name]",
+      inject: "body-last",
+      customDomId: "__svg__icons_dom__"
+    })
   ],
   resolve: {
     alias: {
