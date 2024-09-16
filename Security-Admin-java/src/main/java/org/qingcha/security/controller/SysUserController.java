21d4f6dcbbf229b31b9391749200553227f9af13
QingCha <2521541557@qq.com>
2024-09-16 22:53:19 +0800

    feat✨: 用户管理控制器

--- Security-Admin-java/src/main/java/org/qingcha/security/controller/SysUserController.java
+++ Security-Admin-java/src/main/java/org/qingcha/security/controller/SysUserController.java
@@ -1,4 +1,33 @@
 package org.qingcha.security.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.qingcha.security.common.result.AjaxResult;
+import org.qingcha.security.entity.SysUser;
+import org.qingcha.security.service.SysUserService;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 用户管理
+ * @author QingCha
+ */
+@RestController
+@RequestMapping("/sys/user")
+@RequiredArgsConstructor
 public class SysUserController {
+    private final SysUserService sysUserService;
+
+    /**
+     * 添加用户
+     * @param sysUser 用户数据
+     * @return AjaxResult
+     */
+    @PostMapping("/insert")
+    public AjaxResult insertUser(@RequestBody SysUser sysUser) {
+        boolean save = sysUserService.save(sysUser);
+        if (save) {
+            return AjaxResult.success();
+        }
+        return AjaxResult.error();
+    }
+
 }
