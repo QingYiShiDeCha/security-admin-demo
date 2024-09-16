7006b9abae42f7e04dc36a1b8ac47ba16935c309
QingCha <2521541557@qq.com>
2024-09-16 15:18:38 +0800

    feat✨: 登录页

--- Security-Admin-Vue/src/router/index.js
+++ Security-Admin-Vue/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,6 +8,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/login',
+      name: 'login',
+      meta: { title: '登录页' },
+      component: () => import('@/views/Login/index.vue')
+    }
   ]
 })
 
