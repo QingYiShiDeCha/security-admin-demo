c09f17bf6985f39a17db5fe1026528231973c53a
QingCha <2521541557@qq.com>
2024-09-16 22:52:02 +0800

    fix🐛: 修复代理地址错误

--- Security-Admin-Vue/vite.config.js
+++ Security-Admin-Vue/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8088/',
+        target: 'http://localhost:8088',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
