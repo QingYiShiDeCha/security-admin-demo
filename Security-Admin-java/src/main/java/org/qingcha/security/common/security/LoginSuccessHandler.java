da640db3fef352b00c47730f6f3ec930e4cbda10
QingCha <2521541557@qq.com>
2024-09-16 22:53:03 +0800

    feat✨: 登录成功处理器日志

--- Security-Admin-java/src/main/java/org/qingcha/security/common/security/LoginSuccessHandler.java
+++ Security-Admin-java/src/main/java/org/qingcha/security/common/security/LoginSuccessHandler.java
@@ -3,6 +3,7 @@ package org.qingcha.security.common.security;
 import com.alibaba.druid.support.json.JSONUtils;
 import com.alibaba.fastjson.JSON;
 import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+import lombok.extern.slf4j.Slf4j;
 import org.qingcha.security.common.result.AjaxResult;
 import org.qingcha.security.common.utils.JwtUtils;
 import org.springframework.security.core.Authentication;
@@ -15,12 +16,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 登录成功处理器
  * @author QingCha
  */
 @Component
+@Slf4j
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -28,13 +32,16 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
         ServletOutputStream outputStream = response.getOutputStream();
 
         String username = "user";
-
+        log.info("进入登录处理器");
         try {
             String token = JwtUtils.genrateJwtToken(username);
 
-            Object put = AjaxResult.success("登录成功").put("token", token);
+            Map<String, Object> map = new HashMap<>();
+            map.put("token", token);
+            map.put("code", HttpServletResponse.SC_OK);
+            map.put("message", "登录成功");
 
-            outputStream.write(JSONUtils.toJSONString(put).getBytes(StandardCharsets.UTF_8));
+            outputStream.write(JSONUtils.toJSONString(map).getBytes(StandardCharsets.UTF_8));
             outputStream.flush();
             outputStream.close();
         } catch (Base64DecodingException e) {
