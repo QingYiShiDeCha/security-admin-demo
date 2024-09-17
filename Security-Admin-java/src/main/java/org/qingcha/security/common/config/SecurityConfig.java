8021a4120ebfbc1e6ab0af9fd686de550a5bc431
QingCha <2521541557@qq.com>
2024-09-17 14:06:33 +0800

    feat✨: 引入响应配置

--- Security-Admin-java/src/main/java/org/qingcha/security/common/config/SecurityConfig.java
+++ Security-Admin-java/src/main/java/org/qingcha/security/common/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package org.qingcha.security.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.qingcha.security.common.security.JwtLogoutSuccessHandler;
 import org.qingcha.security.common.security.LoginFailureHandler;
 import org.qingcha.security.common.security.LoginSuccessHandler;
 import org.qingcha.security.common.security.UserDetailsServiceImpl;
+import org.qingcha.security.common.security.filter.JwtAuthenticationEntryPoint;
+import org.qingcha.security.common.security.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -27,9 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final LoginSuccessHandler loginSuccessHandler;
     private final LoginFailureHandler loginFailureHandler;
     private final UserDetailsServiceImpl userDetailsService;
+    private final JwtLogoutSuccessHandler jwtLogoutSuccessHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     private final String[] WHITELIST = {
-            "/login", "/logout", "/captcha", "/password", "/image/**", "/sys/user/insert"
+            "/login", "/logout", "/captcha", "/password", "/image/**"
     };
 
     @Override
@@ -45,10 +50,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .csrf()
                 .disable()
-        // 登录登录配置
+        // 登录登出配置
             .formLogin()
                 .successHandler(loginSuccessHandler)
                 .failureHandler(loginFailureHandler)
+            .and()
+                .logout()
+                .logoutSuccessHandler(jwtLogoutSuccessHandler)
         // session禁用
             .and()
                 .sessionManagement()
@@ -57,14 +65,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .authorizeRequests()
                 .antMatchers(WHITELIST).permitAll()  // 方向白名单
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
         // 异常配置
+            .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
 
         // 自定义过滤器配置
+            .and()
+                .addFilter(jwtAuthenticationFilter());
     }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        return new JwtAuthenticationFilter(authenticationManager());
     }
 }
