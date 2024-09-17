0514b215bfc55a93389914e3e1103d71d9bb5fdb
QingCha <2521541557@qq.com>
2024-09-17 14:10:58 +0800

    feat✨: 退出方法测试

--- Security-Admin-Vue/src/layout/index.vue
+++ Security-Admin-Vue/src/layout/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <el-button type="primary" @click="getUserList">接口测试</el-button>
+    <el-col>
+      <el-button type="primary" @click="getUserList">接口测试</el-button>
+      <el-button type="primary" @click="doLogout">退出测试</el-button>
+    </el-col>
   </div>
 </template>
 
 <script setup>
 import { ListUser } from '@/api/user'
+import { logout } from '@/api/auth'
+import { useAuthStore } from '@/stores/auth'
+
+const { removeToken } = useAuthStore()
+
 async function getUserList() {
   const res = await ListUser()
   console.log(res)
+}
+
+async function doLogout() {
+  const res = await logout()
+  console.log('退出res', res)
+  removeToken()
 }
 
 </script>
