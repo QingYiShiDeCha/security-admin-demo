b609c6e7d0094730cb4cc552315a1d7620ba03b6
QingCha <2521541557@qq.com>
2024-09-17 14:04:19 +0800

    feat✨: 退出成功处理器

--- Security-Admin-java/src/main/java/org/qingcha/security/common/security/JwtLogoutSuccessHandler.java
+++ Security-Admin-java/src/main/java/org/qingcha/security/common/security/JwtLogoutSuccessHandler.java
@@ -1,2 +1,30 @@
-package org.qingcha.security.common.security;public class LogoutSuccessHandler {
+package org.qingcha.security.common.security;
+
+import com.alibaba.fastjson.JSON;
+import org.qingcha.security.common.result.AjaxResult;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 自定义logout处理
+ */
+@Component
+public class JwtLogoutSuccessHandler implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.setContentType("application/json;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        writer.write(JSON.toJSONString(AjaxResult.success("退出成功!")));
+        writer.flush();
+        writer.close();
+    }
 }
