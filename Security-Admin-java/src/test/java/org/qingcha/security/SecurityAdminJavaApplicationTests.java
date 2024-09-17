466b29ad72aebbb6299eaebcee00a5b5cbe56ce3
QingCha <2521541557@qq.com>
2024-09-17 14:16:50 +0800

    feat✨: 啥也不是

--- Security-Admin-java/src/test/java/org/qingcha/security/SecurityAdminJavaApplicationTests.java
+++ Security-Admin-java/src/test/java/org/qingcha/security/SecurityAdminJavaApplicationTests.java
@@ -2,12 +2,16 @@ package org.qingcha.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class SecurityAdminJavaApplicationTests {
 
+
     @Test
     void contextLoads() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println(bCryptPasswordEncoder.encode("123456"));
     }
 
 }
