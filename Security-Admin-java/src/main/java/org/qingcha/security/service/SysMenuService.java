6f675b3b969da073ae3998f798489ae7a48b374e
QingCha <2521541557@qq.com>
2024-09-18 19:27:25 +0800

    feat✨: 菜单业务类

--- Security-Admin-java/src/main/java/org/qingcha/security/service/SysMenuService.java
+++ Security-Admin-java/src/main/java/org/qingcha/security/service/SysMenuService.java
@@ -3,6 +3,8 @@ package org.qingcha.security.service;
 import org.qingcha.security.entity.SysMenu;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
 * @author QingCha
 * @description 针对表【sys_menu】的数据库操作Service
@@ -10,4 +12,18 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface SysMenuService extends IService<SysMenu> {
 
+    /**
+     * 查找用户菜单
+     *
+     * @param userId 用户id
+     * @return List
+     */
+    List<SysMenu> findMenuByUserId(Long userId);
+
+    /**
+     * 查找菜单列表
+     *
+     * @return List
+     */
+    List<SysMenu> queryMenuList();
 }
