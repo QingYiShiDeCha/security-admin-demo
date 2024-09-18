9453ffab46f4cdbd31b7430b860e2317aa3cb47d
QingCha <2521541557@qq.com>
2024-09-18 19:25:56 +0800

    feat✨: 导出组件

---
+++ Security-Admin-Vue/src/layout/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './header/index.vue'
+export { default as Main } from './main/index.vue'
+export { default as Tabs } from './tabs/index.vue'
+export { default as Menu } from './menu/index.vue'
