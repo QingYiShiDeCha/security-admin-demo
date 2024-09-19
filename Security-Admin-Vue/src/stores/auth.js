be60c0fd960d1e2123812b7628867757434760a0
QingCha <2521541557@qq.com>
2024-09-19 17:31:20 +0800

    refactor🎨: 取消登录方法

--- Security-Admin-Vue/src/stores/auth.js
+++ Security-Admin-Vue/src/stores/auth.js
@@ -1,6 +1,5 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
-import { login } from '@/api/auth'
 
 export const useAuthStore = defineStore('auth', () => {
   const token = ref('')
@@ -24,24 +23,10 @@ export const useAuthStore = defineStore('auth', () => {
     token.value = null
   }
 
-  /**
-   * 登录方法
-   * 
-   * @param {{}} data 登录数据
-   * @param {string} data.username 用户名
-   * @param {string} data.password 密码
-   */
-  async function doLogin(data) {
-    const res = await login(data)
-    console.log('login res', res)
-    setToken(res.token)
-  }
-
   return {
     token,
     getToken,
     setToken,
-    doLogin,
     removeToken
   }
 }, {
