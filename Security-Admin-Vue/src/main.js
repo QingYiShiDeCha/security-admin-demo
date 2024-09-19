eca0a3c52922aa3d2e10cf30a005aec736eeae51
QingCha <2521541557@qq.com>
2024-09-19 20:57:52 +0800

    feat✨: 全局使用饿了么icon组件

--- Security-Admin-Vue/src/main.js
+++ Security-Admin-Vue/src/main.js
@@ -6,11 +6,17 @@ import { createPinia } from 'pinia'
 import pinaPluginPersist from 'pinia-plugin-persist'
 
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import 'virtual:svg-icons-register'
 const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 const pinia = createPinia()
 pinia.use(pinaPluginPersist)
 app.use(ElementPlus)
